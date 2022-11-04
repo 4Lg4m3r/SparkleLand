28f8ab8bbeeb5b33d648436c4e6f244f7af20eb3
ᴀᴇʟ <100769378+Aelita05@users.noreply.github.com>
2022-11-04 10:23:22 +0200

    Camera 1.0
    aka mouse looking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,15 +15,34 @@ public class PlayerMovement : MonoBehaviour
 		{
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
 			transform.position += Vector3.forward * speed * Time.deltaTime;
 		}
+		if (Input.GetKey(KeyCode.W))
+			{
+			transform.position += Vector3.forward * speed * Time.deltaTime;
+		}
+
 		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.back * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S))
 		{
 			transform.position += Vector3.back * speed * Time.deltaTime;
 		}
