e855e5d6fff7598e848a82967d3102d5605fe05e
Aden <100773578+4Lg4m3r@users.noreply.github.com>
2022-11-24 10:04:22 +0200

    Spawn created

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public Transform[] m_SpawnPoints;
+    public GameObject m_EnemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNewEnemy();
+    }
+
+    void SpawnNewEnemy()
+    {
+        int randomNumber = Mathf.RoundToInt(Random.Range(0f, m_SpawnPoints.Length-1));
+
+        Instantiate(m_EnemyPrefab, m_SpawnPoints[randomNumber].transform.position, Quaternion.identity);
+    }
+}
